6b0ebd3da6196329c22a6dfc7f4c3865b11605ce
UPPolaris <65070130@kmitl.ac.th>
2022-11-23 22:50:09 +0700

    Co-authored-by: ScorpionKubb <ScorpionKubb@users.noreply.github.com> Co-authored-by: Pakin Junjumlong <65070174@kmitl.ac.th>

--- test.py
+++ test.py
@@ -1,3 +1,13 @@
-print("test")
-print("tset2w")
-print("test3")
+import pygame
+pygame.init()
+screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("Test Game")
+bg = pygame.image.load("bg.jpg")
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            quit()
+
+    screen.blit(bg, (0, 0))
+    pygame.display.update()
