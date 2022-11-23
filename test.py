c6d1515ff978244267f6da1192b5f8bb70b523cf
UPPolaris <65070130@kmitl.ac.th>
2022-11-23 23:20:55 +0700

    screen appear

--- test.py
+++ test.py
@@ -1,13 +1,15 @@
 import pygame
+import random
+import time
+
 pygame.init()
 screen = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("Test Game")
-bg = pygame.image.load("bg.jpg")
+bg = pygame.image.load("bg2.jpg")
+screen.blit(bg, (0, 50)) #ตั้งให้ห่างจากขอบซ้าย,บน
+pygame.display.update()
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             quit()
-
-    screen.blit(bg, (0, 0))
-    pygame.display.update()
