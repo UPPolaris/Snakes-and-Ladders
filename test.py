2c9afeee004a74e3829af355237fd7322544f796
UPPolaris <65070130@kmitl.ac.th>
2022-11-26 23:49:43 +0700

    add dice image and set position to dice image
    Co-authored-by: ScorpionKubb <ScorpionKubb@users.noreply.github.com>
    Co-authored-by: Puckpp <Puckpp@users.noreply.github.com>
    Co-authored-by: Pakin Junjumlong <65070174@kmitl.ac.th>

--- test.py
+++ test.py
@@ -3,11 +3,20 @@ import random
 import time
 
 pygame.init()
-screen = pygame.display.set_mode((800, 600))
+screen = pygame.display.set_mode((1000, 600)) #สร้าง&ตั้งค่าขนาดหน้าจอ
 pygame.display.set_caption("Test Game")
-bg = pygame.image.load("bg2.jpg")
-screen.blit(bg, (0, 50)) #ตั้งให้ห่างจากขอบซ้าย,บน
+table = pygame.image.load("table_1.png")
+screen.blit(table, (0, 0)) #ตั้งให้ห่างจากขอบซ้าย,บน
+
+dice = pygame.image.load("1diceface.png")
+screen.blit(dice, (700, 40))
+
 pygame.display.update()
+#movement
+#now_po = 1
+#dice = random.randint(1,6)
+#new_po = now_po + dice
+
 
 while True:
     for event in pygame.event.get():
